158557690d6f6adb8621a4259dd4f17a8e7cc4d9
spidΞy <web3slinger@gmail.com>
2022-07-07 09:42:42 -0500

    Create room and message server

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,67 @@
+const http = require('http')
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
-const PORT = process.env.PORT || 8080
-const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
+const server = http.createServer(app)
 const io = require('socket.io')(server, {
   cors: {
     origin: '*',
   },
 })
-console.log('socket.io server running')
+app.use(cors())
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./user')
 
-io.sockets.on('connection', (socket) => {
-  console.log('new connection')
+io.on('connection', (socket) => {
+  socket.on('join_room', (room, address) => {
+    socket.join(room)
+
+    const { user } = addUser({
+      id: socket.id,
+      address: address,
+      room: room,
+    })
+
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    })
+    // callback()
+  })
+
+  socket.on('sendMessage', (message) => {
+    const user = getUser(socket.id)
+    io.to(user.room).emit('message', { sender: user.address, content: message })
+
+    // // If the user disconnects resend the room data to client
+    // io.to(user.room).emit('roomData', {
+    //   room: user.room,
+    //   users: getUsersInRoom(user.room),
+    // })
+    // callback()
+  })
 
   socket.on('draw', (data) => {
-    console.log('draw')
-    socket.broadcast.emit('draw', data)
+    const user = getUser(socket.id)
+    io.to(user.room).emit('draw', data)
+  })
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id)
+
+    if (user) {
+      io.to(user.room).emit('message', {
+        sender: '',
+        content: `${user.address} had left the game`,
+      })
+
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      })
+    }
   })
 })
+
+server.listen(process.env.PORT || 3001, () =>
+  console.log(`Server has started.`)
+)
---
+++ server/src/user.js
@@ -0,0 +1,28 @@
+const users = []
+
+const addUser = ({ id, address, room }) => {
+  address = address.trim().toLowerCase()
+  room = room.trim().toLowerCase()
+
+  const existingUser = users.find((user) => {
+    user.room === room && user.address === address
+  })
+
+  if (existingUser) return { error: 'Address is already connected!' }
+  const user = { id, address, room }
+
+  users.push(user)
+  return { user }
+}
+
+const removeUser = (id) => {
+  const index = users.map((user) => user.id).indexOf(id)
+
+  if (index !== -1) return users.splice(index, 1)[0]
+}
+
+const getUser = (id) => users.find((user) => user.id === id)
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room)
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom }
