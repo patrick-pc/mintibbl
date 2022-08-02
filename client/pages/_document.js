a4b8f5bb4c4ea46ff899e1f9f9aff1fe71597464
spidΞy <web3slinger@gmail.com>
2022-08-02 11:11:06 -0500

    Add google tag manager script

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,21 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 class AppDocument extends Document {
   render() {
     return (
       <Html lang='en'>
         <Head>
-          <Script id='google-analytics' strategy='lazyOnload'>
-            {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-                `}
-          </Script>
           <link rel='preconnect' href='https://vitals.vercel-insights.com' />
           <link
             href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
@@ -29,6 +18,23 @@ class AppDocument extends Document {
           <meta
             name='twitter:image'
             content='https://mintibbl.fun/img/dino.png'
+          />
+          <script
+            async
+            src='https://www.googletagmanager.com/gtag/js?id=G-TVHTQ2DMPW'
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-TVHTQ2DMPW', {
+                page_path: window.location.pathname
+              });
+            `,
+            }}
           />
         </Head>
         <body>
